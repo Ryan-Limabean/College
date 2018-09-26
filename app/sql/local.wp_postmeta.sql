5e60b7bdf00c986e52842332ec5b731faa818ca7
ryan-limabean <ryan@limabean.co.za>
2018-09-26 13:57:33 +0200

    db dump

--- app/sql/local.wp_postmeta.sql
+++ app/sql/local.wp_postmeta.sql
@@ -3,4 +3,12 @@
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_postmeta` VALUES
 (1,2,"_wp_page_template","default"),
-(2,3,"_wp_page_template","default");
+(2,3,"_wp_page_template","default"),
+(3,5,"_edit_last","1"),
+(4,5,"_edit_lock","1537436210:1"),
+(7,7,"_edit_last","1"),
+(8,7,"_edit_lock","1537436231:1"),
+(9,10,"_wp_attached_file","2018/09/dobchange.png"),
+(10,9,"_edit_last","1"),
+(11,9,"_edit_lock","1537957958:1"),
+(12,10,"_wp_attachment_metadata","a:5:{s:5:\"width\";i:639;s:6:\"height\";i:45;s:4:\"file\";s:21:\"2018/09/dobchange.png\";s:5:\"sizes\";a:2:{s:9:\"thumbnail\";a:4:{s:4:\"file\";s:20:\"dobchange-150x45.png\";s:5:\"width\";i:150;s:6:\"height\";i:45;s:9:\"mime-type\";s:9:\"image/png\";}s:6:\"medium\";a:4:{s:4:\"file\";s:20:\"dobchange-300x21.png\";s:5:\"width\";i:300;s:6:\"height\";i:21;s:9:\"mime-type\";s:9:\"image/png\";}}s:10:\"image_meta\";a:12:{s:8:\"aperture\";s:1:\"0\";s:6:\"credit\";s:0:\"\";s:6:\"camera\";s:0:\"\";s:7:\"caption\";s:0:\"\";s:17:\"created_timestamp\";s:1:\"0\";s:9:\"copyright\";s:0:\"\";s:12:\"focal_length\";s:1:\"0\";s:3:\"iso\";s:1:\"0\";s:13:\"shutter_speed\";s:1:\"0\";s:5:\"title\";s:0:\"\";s:11:\"orientation\";s:1:\"0\";s:8:\"keywords\";a:0:{}}}");
--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -16,4 +16,9 @@ INSERT INTO `wp_usermeta` VALUES
 (12,1,"wp_capabilities","a:1:{s:13:\"administrator\";b:1;}"),
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers","wp496_privacy"),
-(15,1,"show_welcome_panel","1");
+(15,1,"show_welcome_panel","1"),
+(16,1,"session_tokens","a:1:{s:64:\"198a06d58dbd55b4d5fce8d6929d53d2c84707b3a40b0ae71c8def3b199b3be8\";a:4:{s:10:\"expiration\";i:1538126054;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:115:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36\";s:5:\"login\";i:1537953254;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","4"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"wp_user-settings","libraryContent=browse"),
+(20,1,"wp_user-settings-time","1537956294");
